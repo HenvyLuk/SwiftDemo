88f96667744e392368238b4e7e8a49821a3ba360
HenvyLuk <henvyluk@163.com>
2016-05-27 12:05:31 +0800

    henvy dymatic

--- SwiftDemo/SwiftDemo/ViewController.swift
+++ SwiftDemo/SwiftDemo/ViewController.swift
@@ -9,17 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myImage: UIImageView!
+    
+    var dynamicAni = UIDynamicAnimator()
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        test()
+        dynamics()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func dynamics() {
+        dynamicAni = UIDynamicAnimator(referenceView: self.view)
+        let gravity = UIGravityBehavior(items: [self.myImage])
+        dynamicAni.addBehavior(gravity)
+        
+        let collsion = UICollisionBehavior(items: [self.myImage])
+        collsion.translatesReferenceBoundsIntoBoundary = true
+        dynamicAni.addBehavior(collsion)
+        
     }
+    
+    
+    func test() {
+        let array = [2000,541,34,78,999]
+        let newArray = array.sort()
 
-
+        print(newArray)
+        
+       
+    }
+    
 }
 
